aacbb5d06e88d269ca818c9916fc1f34c12554dc
Emmanoel Porto <emmanoelporto@gmail.com>
2022-07-21 22:28:16 -0300

    fix-lint: fix double quotes in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>
         Hello World :)
       </h1>
