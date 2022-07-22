cef3497f3690ea7baad6e95029d68d2a73ed0903
Emmanoel Porto <emmanoelporto@gmail.com>
2022-07-22 02:57:22 -0300

    feat: Render All User Names on the screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import allUsers from './endpoints/allUsers';
+import useFetch from './Hooks/useFetch';
 
 function App() {
+
+  const [ data ] = useFetch(allUsers);
+
+  const app = !data ? ('load') : data.map(repos => {
+  
+    return(
+      <li key={repos.name}>
+        <p>{repos.name}</p>
+      </li>
+    )
+  })
+
   return (
     <div className='App'>
-      <h1>
-        Hello World :)
-      </h1>
+        {app}
     </div>
   );
 }
