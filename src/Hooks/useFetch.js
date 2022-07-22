db7ea5769eb42c45c1c04a1c0b55cfc981b0e60b
Emmanoel Porto <emmanoelporto@gmail.com>
2022-07-22 02:21:33 -0300

    remove: eslint config files

---
+++ src/Hooks/useFetch.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useFetch = (url) => {
+
+    const [result, setResult] = useState([]);
+
+    useEffect(() => {
+
+        axios.get(url)
+        .then(response => { setResult(response.data) })
+
+    }, [url])
+
+    return { result };
+
+}
+
+export default useFetch;
