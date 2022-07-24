58a4244118b67ea2abef763488efbb0d2d0ab673
Emmanoel Porto <emmanoelporto@gmail.com>
2022-07-24 15:49:49 -0300

    feat: new Branch Cards Component to render branchs

---
+++ src/components/RepoBranchesPage/BranchCards.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import cardImg from '../../images/laptop.webp'
+import '../ReposPage/repos.css'
+import cardAlt from '../../staticTexts/cardAlt';
+// import { useNavigate } from 'react-router-dom';
+
+function BranchCards({ res, repo }) {
+    console.log(res, repo);
+
+  const name = repo;
+
+//   const navigate = useNavigate();
+
+  return (
+  <section className="section">
+  
+    <div className="container">
+      <h3 className="title has-text-centered is-size-4">
+
+        {`${name} Repositories:`}
+
+      </h3>
+      <div className="mt-5 columns is-centered is-8 is-variable">
+        <div className="column is-4-tablet is-3-desktop">
+
+        {res.map((repo) => (
+          <div className="card mb-2 " key={ repo.name }>
+            <div className="card-image has-text-centered px-6 ">
+              <img src={cardImg} alt={cardAlt } />
+            </div>
+            <div className="card-content">
+              <p className="title is-size-6 has-text-centered">{ repo.name }</p>
+            </div>
+            <footer className="card-footer">
+              <button
+                className="button card-footer-item is-primary is-rounded has-text-grey mb-3"
+                onClick={() => {
+                //   navigate(`/${repo.owner.login}/repos/${repo.name}`);
+                }}
+              >
+                View Commits
+              </button>
+            </footer>
+          </div>
+        ))}
+
+        </div>
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default BranchCards;
