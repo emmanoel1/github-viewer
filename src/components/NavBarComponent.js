9617e5949de3b020d7fd02799845d3e859afb0a4
Emmanoel Porto <emmanoelporto@gmail.com>
2022-07-29 18:19:56 -0300

    feat: new navbar animation on mobile

--- src/components/NavBarComponent.js
+++ src/components/NavBarComponent.js
@@ -1,30 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import GitLogo from '../images/gitLogo.png';
 import GitLogoAlt from "../staticTexts/GitLogoAlt";
 
 function NavBarComponent() {
+  const [isActive, setIsActive] = useState(false);
+
+  const navMobileClick = () => {
+    if(isActive === false){
+      setIsActive(true);
+    }
+    if(isActive === true){
+      setIsActive(false);
+    }
+  }
+
   return(
     <nav className="navbar is-fixed-top">
       <div className="container">
+
         <div className="navbar-brand">
+
             <a href="https://github.com/" className="navbar-item">
                 <img src={GitLogo} alt={ GitLogoAlt } />
             </a>
-            <p className="navbar-item" >Github Viewer</p>
-            <a role="button" aria-label="menu" aria-expanded="false" href="/" className="navbar-burger" data-target="navbarBasicExample">
+            
+            <button
+            aria-label="menu"
+            aria-expanded="false"
+            className={`navbar-burger ${isActive ? 'is-active' : ''}`}
+            onClick={navMobileClick}
+            >
                 <span aria-hidden="true" />
                 <span aria-hidden="true" />
                 <span aria-hidden="true" />
-            </a>
+            </button>
+
         </div>
 
-        <div className="navbar-menu" id="navMenu">
+        <div className={ `navbar-menu ${isActive ? 'is-active' : ''} `}>
             <div className="navbar-start">
+
                 <a href="/" className="navbar-item">
                     Home
                 </a>
+
             </div>
         </div>
+
       </div>
     </nav>
   )
